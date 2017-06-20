e89d7a7a7e580e29c9ef37101ebf3d2f5847d1ff
Elon Azoulay <eazoulay@fb.com>
2017-06-20 01:57:14 -0700

    Rename AbstractResourceGroupConfigurationManager.configureGroup to reconfigureGroup
    To avoid confusion the change the method name for reconfiguring a group to reconfigureGroup

--- presto-resource-group-managers/src/main/java/com/facebook/presto/resourceGroups/db/DbResourceGroupConfigurationManager.java
+++ presto-resource-group-managers/src/main/java/com/facebook/presto/resourceGroups/db/DbResourceGroupConfigurationManager.java
@@ -244,7 +244,7 @@ public class DbResourceGroupConfigurationManager
         for (ResourceGroupIdTemplate resourceGroupIdTemplate : changedSpecs) {
             for (ResourceGroupId resourceGroupId : configuredGroups.getOrDefault(resourceGroupIdTemplate, ImmutableList.of())) {
                 synchronized (getRootGroup(resourceGroupId)) {
-                    configureGroup(groups.get(resourceGroupId), resourceGroupSpecs.get(resourceGroupIdTemplate));
+                    reconfigureGroup(groups.get(resourceGroupId), resourceGroupSpecs.get(resourceGroupIdTemplate));
                 }
             }
         }
